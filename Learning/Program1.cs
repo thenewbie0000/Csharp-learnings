11131aa3704b6ec3e73da16344f2c68d48282953
Rojesh <shrestharojesh34@gmail.com>
2024-03-10 18:34:45 +0545

    Add project files.

---
+++ Learning/Program1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Learning
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
