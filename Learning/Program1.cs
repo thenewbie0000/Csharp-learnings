85e56109def4b3df027cb9411e4a3eccd2037ebf
Rojesh <shrestharojesh34@gmail.com>
2024-03-10 19:43:41 +0545

    Started Learning

--- Learning/Program1.cs
+++ Learning/Program1.cs
@@ -6,12 +6,28 @@ namespace Learning
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Blue;
+            /*Console.ForegroundColor = ConsoleColor.Blue;
             Console.BackgroundColor = ConsoleColor.White;
             Console.Clear();
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
-            Console.WriteLine($"Hello {name}");
+            Console.WriteLine($"Hello {name}");*/
+
+            Console.WriteLine("Biggest Integer: {0}", int.MaxValue);
+            Console.WriteLine("Smallest Integer: {0}", int.MinValue);
+
+            Console.WriteLine("Biggest Long: {0}", long.MaxValue);
+            Console.WriteLine("Smallest Integer: {0}", long.MinValue);
+
+            Console.WriteLine("Biggest Decimal: {0}", decimal.MaxValue);
+            Console.WriteLine("Smallest Decimal: {0}", decimal.MinValue);
+
+            decimal decPiValue = 3.142857142857143M;
+            decimal decBigNum = 3.0000000000000000000000011M;
+            Console.WriteLine("DECIMAL: PI + bigNum = {0}", decPiValue + decBigNum);
+            Console.ReadLine();
+
+ 
         }
     }
 }
